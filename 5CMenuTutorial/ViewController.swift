8e1ee317f014a4f6bd8d01f531e1b935de58a9c2
Maddie Zug <madelinezug@gmail.com>
2016-11-03 23:13:53 -0700

    Initial Commit

---
+++ 5CMenuTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  5CMenuTutorial
+//
+//  Created by Maddie on 11/3/16.
+//  Copyright © 2016 Maddie Zug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
